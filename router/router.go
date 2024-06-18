8a9ca22cf5925541142c322561d9df1d61de9c70
Diekkan <merentiel.diego@gmail.com>
2024-06-18 08:44:30 -0300

    Routing file

--- router/router.go
+++ router/router.go
@@ -1,1 +1,13 @@
-package file_manager
+package router
+
+import (
+	"filemanager/handlers"
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/list-files", handlers.ListFilesOnDirectory).Methods("GET")
+	router.HandleFunc("/upload-file", handlers.UploadFile).Methods("POST")
+	return router
+}
