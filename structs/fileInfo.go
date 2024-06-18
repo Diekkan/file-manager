074a40f4368fc52f074893617defa749c8d1bea6
Diekkan <merentiel.diego@gmail.com>
2024-06-18 08:45:00 -0300

    File Info struct

--- structs/fileInfo.go
+++ structs/fileInfo.go
@@ -1,1 +1,8 @@
 package structs
+
+type FileInfo struct {
+	Name      string `json: "Name"`
+	Extension string `json: "Extension"`
+	Size      int64  `json: "Size"`
+	IsDir     bool   `json: "IsDir"`
+}
