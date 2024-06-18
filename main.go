57c751cf6288191d12b8bff085d2254c208a45a9
Diekkan <merentiel.diego@gmail.com>
2024-06-18 08:43:48 -0300

    Main package

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package file_manager
+package main
+
+import (
+	"filemanager/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.SetupRouter()
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
